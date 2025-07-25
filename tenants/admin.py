289b320e38add8f34601b2f54a9a7e64a94c56b8
Stefano Incarico <stefano.incarico@outlook.it>
2025-07-25 06:50:51 +0200

    feat: Create superuser and register tenant models in admin panel

--- tenants/admin.py
+++ tenants/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+# tenants/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Company, UserCompanyPermission
+
+# La registrazione più semplice possibile.
+# Dice a Django: "Mostra questo modello nel pannello di amministrazione".
+admin.site.register(Company)
+admin.site.register(UserCompanyPermission)
